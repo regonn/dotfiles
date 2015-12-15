99cb6121b89e66425b00add5e8a97edbef2e684b
regonn <github@regonn.tokyo>
2015-12-15 16:17:09 +0900

    add eslitrc.json

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.xvimrc ~/.xvimrc
 ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
+ln -s ~/dotfiles/.eslintrc.json ~/.eslintrc.json
