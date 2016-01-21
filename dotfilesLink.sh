ebcf67e0aa834546a553e9263126fda7c2934ce8
regonn <github@regonn.tokyo>
2016-01-21 13:09:21 +0900

    sass lint 追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,4 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.xvimrc ~/.xvimrc
 ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 ln -s ~/dotfiles/.eslintrc.json ~/.eslintrc.json
+ln -s ~/dotfiles/.sass-lint.yml ~/.sass-lint.yml
